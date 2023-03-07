8ac853c0a0986ceb2066e8b487dc7b1236738dc2
anujayaranathunge <101706703+anujayaranathunge@users.noreply.github.com>
2023-03-07 22:08:54 +0700

    home
    home done

--- Front-End/projectfe/src/components/Footer.js
+++ Front-End/projectfe/src/components/Footer.js
@@ -7,7 +7,14 @@ const Footer = () => {
     <footer className='bg-dark text-light'>
       <Container>
         <Row>
-          <Col md={4}  >
+
+          <Col md={4}>
+            <br/>
+          <h3 className='text-warning'>NerveSeg-US</h3>
+          <p>Identification of source of pain through nerve segmentation of ultrasound medical images.</p>
+          </Col>
+
+          <Col md={3}  >
             <br/>
             <h5 className='text-warning'>Get In Touch</h5>
             <br/>
@@ -40,18 +47,20 @@ const Footer = () => {
                 />{' '}
                 +94778694522</p>
           </Col>
-          <Col md={4}>
+
+          <Col md={2}>
           <br/>
             <h5 className='text-warning'>Quick Links</h5>
             <ul className="list-unstyled, variant-dark">
               <li><a href="/home" class="text-light text-decoration-none">Home</a></li>
               <li><a href="/about" class="text-light text-decoration-none">About</a></li>
-              <li><a href="/doctor" class="text-light text-decoration-none">Doctor</a></li>
-              <li><a href="/patient" class="text-light text-decoration-none">Patient</a></li>
+              <li><a href="/doctorlog" class="text-light text-decoration-none">Doctor</a></li>
+              <li><a href="/patientlog" class="text-light text-decoration-none">Patient</a></li>
               <li><a href="/contact" class="text-light text-decoration-none">Contact</a></li>
             </ul>
           </Col>
-          <Col md={4}>
+
+          <Col md={3}>
           <br/>
             <h5 className='text-warning'>Follow Us</h5>
             <br/>
@@ -95,7 +104,6 @@ const Footer = () => {
                 />
                 </a>
                 
-           
           </Col>
         </Row>
         <Row>
--- Front-End/projectfe/src/pages/home.js
+++ Front-End/projectfe/src/pages/home.js
@@ -1,8 +1,62 @@
 import React from "react";
+import { Container, Row, Col, Button,Image, Link } from 'react-bootstrap';
+import './home.css';
+
 
 export const Home = () =>{
     return(
-        <>
+    <>
+      <Row>
+        <Col>
+        <br/>
+        <div className="welcometext">
+          <h4>Welcome to</h4>
+          <h1>NerveSeg-US</h1>
+        </div>
+        </Col>
+        
+        <Col className="col01">
+            <br/>
+          <Col>
+          <div className="homeicone">
+          <img
+                  alt=""
+                  src="doctor.png"
+                  width="250"
+                  height="242"
+                />
+                <br/>
+                <br/>
+               <h4 className="texthome">Sign in as a Doctor</h4> 
+               <a href="/doctorlog" class="text-light text-decoration-none">
+               <Button className="buttonSinein">SIGN IN</Button>
+               </a>
+               </div>
+          </Col>
+          <br/>
+        </Col>
+        <Col className="col01">
+            <br/>
+          <Col>
+          <div className="homeicone">
+          <img
+                  alt=""
+                  src="hospitalisation.png"
+                  width="250"
+                  height="266"
+                />
+                <br/>
+                
+               <h4 className="texthome">Sign in as a Patient</h4> 
+               <a href="/patientlog" class="text-light text-decoration-none">
+               <Button className="buttonSinein">SIGN IN</Button>
+               </a> 
+               </div>
+          </Col>
+        </Col>
+        
+      </Row>
         </>
     )
 }
+export default Home;
