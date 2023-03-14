f72d1420764cb502de718d51f39aedbdffd55fb9
vgshashika <124588073+vgshashika@users.noreply.github.com>
2023-03-14 10:22:24 +0530

    Add files via upload

---
+++ rightside.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Image} from "react-bootstrap";
+
+const RightSide = () => {
+    return (
+        <div>
+           <Image src="imgbg.jpg" thumbnail style={{border:"none"}} /> 
+        </div>
+    )
+}
+
+export default RightSide;
